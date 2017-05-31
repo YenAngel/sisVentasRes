7b7e8f966902c59daf8656b1e29a1a0367aadabc
AnibalMarlo <anibalmarloaguilar@gmail.com>
2017-05-30 22:44:00 -0500

    prueba

---
+++ sgr/src/ventas/modelo/DevClass.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ventas.modelo;
+
+/**
+ *
+ * @author Test
+ */
+public class DevClass {
+    public int tipo;
+    
+    public DevClass(){
+        
+    }
+    public DevClass(int tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+    
+}
